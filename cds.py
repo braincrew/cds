d12c86023132879ea922e35901dbced98a5be0ec
BAEM1N <baemin.dev@gmail.com>
2021-04-21 16:56:41 +0900

    mySUNI 1.0.1

--- cds.py
+++ cds.py
@@ -11,36 +11,87 @@ import os
 dataset_files = {
     '국민연금':{
         'info': '2020년 9월 국민연금 데이터셋',
-        'data': 'http://sk.jaen.kr:8080/national_pension.csv',
-        'filename': 'national_pension.csv',
+        'data': ['http://sk.jaen.kr:8080/datasets/national_pension.csv'],
+        'filename': ['national_pension.csv'],
     },
     '서울시자전거':
     {
         'info': '서울시 따릉이 자전거 대여량 정보',
-        'data': 'http://sk.jaen.kr:8080/seoul_bicycle.csv',
-        'filename': 'seoul_bicycle.csv',
+        'data': ['http://sk.jaen.kr:8080/datasets/seoul_bicycle.csv'],
+        'filename': ['seoul_bicycle.csv'],
+    },
+    '서울시실거래가':
+    {
+        'info': '서울시 아파트 실거래가 정보',
+        'data': ['http://sk.jaen.kr:8080/datasets/2020-seoul-apt-price.csv'],
+        'filename':[ '2020-seoul-apt-price.csv'],
+    },
+    '서울시대중교통':
+    {
+        'info': '서울시 대중교통(지하철, 버스) 이용객 정보',
+        'data': ['http://sk.jaen.kr:8080/datasets/seoul_transportation.xlsx'],
+        'filename':[ 'seoul_transportation.xlsx'],
+    },
+    '서울시주민등록인구':
+    {
+        'info': '서울시 주민등록인구 정보',
+        'data': ['http://sk.jaen.kr:8080/datasets/seoul_population.csv'],
+        'filename': ['seoul_population.csv'],
+    },
+    '유가정보':
+    {
+        'info': '2019년 서울시 유가 정보',
+        'data': ['http://sk.jaen.kr:8080/datasets/gas_first_2019.csv','http://sk.jaen.kr:8080/datasets/gas_second_2019.csv'],
+        'filename':[ 'gas_first_2019.csv', 'gas_second_2019.csv'],
+    },
+    'PandasFileIO':
+    {
+        'info': 'Pandas File I/O 연습 문제용 데이터셋',
+        'data': ['http://sk.jaen.kr:8080/datasets/mySUNI.xlsx','http://sk.jaen.kr:8080/datasets/mySUNI_1.csv',
+        'http://sk.jaen.kr:8080/datasets/mySUNI_2.csv', 'http://sk.jaen.kr:8080/datasets/mySUNI_3.csv'],
+        'filename':[ 'mySUNI.xlsx', 'mySUNI_1.csv', 'mySUNI_2.csv', 'mySUNI_3.csv'],
     }
+
+
 }
 
 files = {
     '자전거 대여량 예측':
     {
-        'train.csv': 'http://sk.jaen.kr:8080/project/bsd/train.csv',
-        'test.csv': 'http://sk.jaen.kr:8080/project/bsd/test.csv',
-        'submission.csv': 'http://sk.jaen.kr:8080/project/bsd/sampleSubmission.csv',
+        'train.csv': 'http://sk.jaen.kr:8080/project/01.Bike-Demand-Prediction/train.csv',
+        'test.csv': 'http://sk.jaen.kr:8080/project/01.Bike-Demand-Prediction/test.csv',
+        'submission.csv': 'http://sk.jaen.kr:8080/project/01.Bike-Demand-Prediction/submission.csv',
     },
     '타이타닉 생존자 예측':
     {
-        'train.csv': 'http://sk.jaen.kr:8080/2020/titanic_train.csv',
-        'test.csv': 'http://sk.jaen.kr:8080/2020/titanic_test.csv',
-        'submission.csv': 'http://sk.jaen.kr:8080/2020/titanic_submission.csv',
+        'train.csv': 'http://sk.jaen.kr:8080/project/00.Titanic/train.csv',
+        'test.csv': 'http://sk.jaen.kr:8080/project/00.Titanic/test.csv',
+        'submission.csv': 'http://sk.jaen.kr:8080/project/00.Titanic/submission.csv',
     },
     '주택 가격 예측':
     {
-        'train.csv': 'http://sk.jaen.kr:8080/2020/titanic_train.csv',
-        'test.csv': 'http://sk.jaen.kr:8080/2020/titanic_test.csv',
-        'submission.csv': 'http://sk.jaen.kr:8080/2020/titanic_submission.csv',
-    }
+        'train.csv': 'http://sk.jaen.kr:8080/project/04.House-Prices-Prediction/train.csv',
+        'test.csv': 'http://sk.jaen.kr:8080/project/04.House-Prices-Prediction/test.csv',
+        'submission.csv': 'http://sk.jaen.kr:8080/project/04.House-Prices-Prediction/submission.csv',
+    },
+    '대출 상환 능력 예측':
+    {
+        'train.csv': 'http://sk.jaen.kr:8080/project/03.Home-Credit-Risk-Classification/train.csv',
+        'test.csv': 'http://sk.jaen.kr:8080/project/03.Home-Credit-Risk-Classification/test.csv',
+        'submission.csv': 'http://sk.jaen.kr:8080/project/03.Home-Credit-Risk-Classification/submission.csv',
+        'bureau.csv': 'http://sk.jaen.kr:8080/project/03.Home-Credit-Risk-Classification/bureau.csv',
+         'bureau_balance.csv': 'http://sk.jaen.kr:8080/project/03.Home-Credit-Risk-Classification/bureau_balance.csv',
+         'credit_card_balance.csv': 'http://sk.jaen.kr:8080/project/03.Home-Credit-Risk-Classification/credit_card_balance.csv',
+         'installments_payments.csv': 'http://sk.jaen.kr:8080/project/03.Home-Credit-Risk-Classification/installments_payments.csv',
+         'POS_CASH_balance.csv': 'http://sk.jaen.kr:8080/project/03.Home-Credit-Risk-Classification/POS_CASH_balance.csv',
+         'previous_application.csv': 'http://sk.jaen.kr:8080/project/03.Home-Credit-Risk-Classification/previous_application.csv'
+    },
+    '웨이퍼 맵 불량 유형 분류':
+    {
+        'train.csv': 'http://sk.jaen.kr:8080/project/05.Wafer-Map-Defect-Classification/train.csv',
+        'test.csv': 'http://sk.jaen.kr:8080/project/05.Wafer-Map-Defect-Classification/test.csv',
+        'submission.csv': 'http://sk.jaen.kr:8080/project/05.Wafer-Map-Defect-Classification/submission.csv',
+    },
 }
 
 ####### 데이터셋 관련 모듈 #######
@@ -74,33 +125,45 @@ class Dataset:
 
     def info(self):
         display(self.dataset[['name', 'info', 'filename']])
-        
-        
-    def load(self, dataset_name):
-        global dataset_files
-        fileurl = dataset_files[dataset_name]['data']
-        filename = dataset_files[dataset_name]['filename']
 
-        # auth
+
+    def load(self, dataset_names):
+        global dataset_files
         username = 'mysuni'
         password = 'mysuni1!'
 
-        r = requests.get(fileurl, auth=HTTPBasicAuth(username, password))
-        filepath = os.path.join(self.data_dir, filename)
-        open(filepath, 'wb').write(r.content)
-        print(f'파일 다운로드 완료\n====================\n\n데이터셋: {dataset_name}\n파일경로: {filepath}\n\n====================')
-        return filepath
-        
-        
+        if type(dataset_names) == str:
+            fileurl = dataset_files[dataset_names]['data']
+            filename = dataset_files[dataset_names]['filename']
+            for filename, fileurl in zip(filename, fileurl):
+                r = requests.get(fileurl, auth=HTTPBasicAuth(username, password))
+                filepath = os.path.join(self.data_dir, filename)
+                open(filepath, 'wb').write(r.content)
+                print(f'파일 다운로드 완료\n====================\n\n데이터셋: {dataset_names}\n파일경로: {filepath}\n\n====================')
+            return
+        elif type(dataset_names) == list or type(dataset_names) == tuple:
+            for dataset_name in dataset_names:
+                fileurl = dataset_files[dataset_name]['data']
+                filename = dataset_files[dataset_name]['filename']
+                for filename, fileurl in zip(filename, fileurl):
+                    r = requests.get(fileurl, auth=HTTPBasicAuth(username, password))
+                    filepath = os.path.join(self.data_dir, filename)
+                    open(filepath, 'wb').write(r.content)
+                    print(f'파일 다운로드 완료\n====================\n\n데이터셋: {dataset_name}\n파일경로: {filepath}\n\n====================')
+            return
+        else:
+            raise Exception('잘못된 정보입니다.')
+
+
 dataset = Dataset()
 
 
 def list_data():
     global dataset
     dataset.info()
-    
-    
-def load_data(dataset_name):
+
+
+def download_data(dataset_name):
     global dataset
     return dataset.load(dataset_name)
 
@@ -151,30 +214,33 @@ class Project:
         print(self.__project_submission(filename))
 
 
-def load_project(project_name, class_info, email):
+def download_project(project_name, class_info, email):
     global project, files
-    project = Project(project_name, class_info=class_info, email=email)
+    try:
+        project = Project(project_name, class_info=class_info, email=email)
 
-    # data 폴더 경로 지정
-    DATA_DIR = 'data'
+        # data 폴더 경로 지정
+        DATA_DIR = 'data'
 
-    # data 폴더 생성
-    if not os.path.exists(DATA_DIR):
-        os.makedirs(DATA_DIR)
+        # data 폴더 생성
+        if not os.path.exists(os.path.join(DATA_DIR, project_name)):
+            os.makedirs(os.path.join(DATA_DIR, project_name))
 
-    project_files = files[project_name]
+        project_files = files[project_name]
 
-    # auth
-    username = 'mysuni'
-    password = 'mysuni1!'
-    
-    print(f'프로젝트: {project_name}\n==============================\n파일 정보\n')
-    for filename, fileurl in project_files.items():
-        r = requests.get(fileurl, auth=HTTPBasicAuth(username, password))
-        filepath = os.path.join(DATA_DIR, filename)
-        open(filepath, 'wb').write(r.content)
-        print(f'{filename}:\t {filepath}')
-    print(f'\n==============================')
+        # auth
+        username = 'mysuni'
+        password = 'mysuni1!'
+
+        print(f'프로젝트: {project_name}\n==============================\n파일 정보\n')
+        for filename, fileurl in project_files.items():
+            r = requests.get(fileurl, auth=HTTPBasicAuth(username, password))
+            filepath = os.path.join(DATA_DIR, project_name, filename)
+            open(filepath, 'wb').write(r.content)
+            print(f'{filename}:\t {filepath}')
+        print(f'\n==============================')
+    except:
+        raise Exception('잘못된 정보입니다.')
 
 
 def submit(submission_file):
