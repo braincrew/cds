2820179b63383100ce76c74ca3595d66f6ae07c1
BAEM1N <baemin.dev@gmail.com>
2021-06-10 10:08:40 +0900

    tqdm 의존성 추가

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='mySUNI',
-    version='1.1.8',
+    version='1.1.9',
     description='mySUNI CDS',
     author='BAEM1N, Teddy Lee',
     author_email='baemin.dev@gmail.com, teddylee777@gmail.com',
     url='https://github.com/braincrew/cds',
-    install_requires=[],
+    install_requires=['tqdm'],
     packages=find_packages(exclude=[]),
     keywords=['mySUNI', 'CDS'],
     python_requires='>=3',
