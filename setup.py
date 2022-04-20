1f7c43d2d2dab04b860cab1f90ba8b3a57ff23a2
BAEM1N <baemin.dev@gmail.com>
2022-04-20 09:10:28 +0900

    프로젝트 데이터 추가

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mySUNI',
-    version='1.4.1',
+    version='1.4.2',
     description='mySUNI CDS',
     author='BAEM1N, Teddy Lee',
     author_email='baemin.dev@gmail.com, teddylee777@gmail.com',
