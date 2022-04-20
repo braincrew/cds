e8269257004b98e1f0dfeaaf74b9e391a659f46c
BAEM1N <baemin.dev@gmail.com>
2022-04-20 23:18:08 +0900

    지니지수 평가 함수 추가

--- mySUNI/utils.py
+++ mySUNI/utils.py
@@ -382,6 +382,27 @@ def summary(df):
     s['고유값 개수'] = df.nunique().values
     s['고유값'] = [df[col_name].unique() for col_name in df.columns]
     return s
+
+def gini(y_true, y_pred):
+    # 실제값과 예측값의 크기가 같은지 확인 (값이 다르면 오류 발생)
+    if y_true.shape != y_pred.shape:
+        raise Exception('Shape Error : 실제값과 예측값의 개수가 일치하지 않습니다.')
+
+    n_samples = y_true.shape[0]                      # 데이터 개수
+    L_mid = np.linspace(1 / n_samples, 1, n_samples) # 대각선 값
+
+    # 1) 예측값에 대한 지니계수
+    pred_order = y_true[y_pred.argsort()] # y_pred 크기순으로 y_true 값 정렬
+    L_pred = np.cumsum(pred_order) / np.sum(pred_order) # 로렌츠 곡선
+    G_pred = np.sum(L_mid - L_pred)       # 예측 값에 대한 지니계수
+
+    # 2) 예측이 완벽할 때 지니계수
+    true_order = y_true[y_true.argsort()] # y_true 크기순으로 y_true 값 정렬
+    L_true = np.cumsum(true_order) / np.sum(true_order) # 로렌츠 곡선
+    G_true = np.sum(L_mid - L_true)       # 예측이 완벽할 때 지니계수
+
+    # 정규화된 지니계수
+    return G_pred / G_true  
     
 ########## 에러 메시지 ############
 
