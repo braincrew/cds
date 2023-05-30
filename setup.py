31f59fca7bead8070febfbcdd1ed135ea6a91ad0
Teddy Lee <teddylee777@gmail.com>
2023-05-30 09:27:58 +0900

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mySUNI',
-    version='1.6.0',
+    version='2.0.0',
     description='mySUNI CDS',
     author='BAEM1N, Teddy Lee',
     author_email='baemin.dev@gmail.com, teddylee777@gmail.com',
